3ae5a8587ad81ed1e398a515b4df120559fbb19e
x-imp <ashutosh.shinde@niser.ac.in>
2019-08-08 11:32:28 +0530

    finished task

---
+++ q1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "hi...whats your name???"
+read name
+mkdir $name
+touch $name/$name.cpp
--- q2.sh
+++ q2.sh
@@ -1,10 +1,3 @@
-#!/bin/sh
-echo "hi...whats your name???"
-read name
-mkdir $name
-touch $name/$name.cpp
-
-
 echo "hello...enter your name again...."
 read name2
 mkdir $name2"1"
@@ -14,3 +7,4 @@ touch $name2"2".cpp
 touch $name2"3".cpp
 touch $name2"4".cpp
 touch $name2"5".cpp
+
